1430204773d4acde7229e132ac0204539a29cb48
Gesta Gemilang <gestagemilang@Gestas-MacBook-Air.local>
2023-01-21 19:27:40 +0000

    connecting redux with component

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -6,9 +6,9 @@ import {
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
-    case PRODUCT_LIST_SUCCESS:
-      return { loading: true, products: [] };
     case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
