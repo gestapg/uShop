737206c2f044e4ea873d938b4e842b6077ed04cb
Gesta Gemilang <gestagemilang@Gestas-MacBook-Air.local>
2023-01-24 18:30:43 +0000

    create UI for Checkout Steps

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -29,9 +29,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
 const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null;
+const shipppingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {};
 
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shipppingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
